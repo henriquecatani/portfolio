98ddf4fb28fad412efe298538a192cff3d496459
henriquecatani <catanihenrique22@gmail.com>
2025-05-27 22:18:10 -0300

    update

--- js/javascript.js
+++ js/javascript.js
@@ -2,10 +2,14 @@ function dark() {
     document.body.className = "dark"
     document.getElementById("darktg").style.display = "none"
     document.getElementById("lighttg").style.display = ""
+    document.getElementById("darklg").style.display = "none"
+    document.getElementById("lightlg").style.display = ""
 }
 function light() {
     document.body.className = "light"
     document.getElementById("lighttg").style.display = "none"
     document.getElementById("darktg").style.display = ""
+    document.getElementById("darklg").style.display = "none"
+    document.getElementById("lightlg").style.display = ""
 }
 light()
