682c4bf4d2319ad7791791db2021c3b9a116c29c
henriquecatani <catanihenrique22@gmail.com>
2025-05-28 16:36:15 -0300

    Home

--- js/javascript.js
+++ js/javascript.js
@@ -4,12 +4,20 @@ function dark() {
     document.getElementById("lighttg").style.display = ""
     document.getElementById("darklg").style.display = "none"
     document.getElementById("lightlg").style.display = ""
+    localStorage.setItem('modo', '1')
 }
 function light() {
     document.body.className = "light"
     document.getElementById("lighttg").style.display = "none"
     document.getElementById("darktg").style.display = ""
-    document.getElementById("darklg").style.display = "none"
-    document.getElementById("lightlg").style.display = ""
+    document.getElementById("darklg").style.display = ""
+    document.getElementById("lightlg").style.display = "none"
+    localStorage.setItem('modo', '0')
 }
-light()
+
+let mode = localStorage.getItem('modo') || '0'
+if (mode == '0') {
+    light()
+} else {
+    dark()
+}
